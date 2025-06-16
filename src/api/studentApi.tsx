d1fe8d819d4abdb27d992f2c3a7ad1abe83748ab
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2025-06-16 19:11:54 +0530

    we write api endpoint for calling login api

---
+++ src/api/studentApi.tsx
@@ -0,0 +1,29 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+export const studentApi = createApi({
+    reducerPath : "studentApi",
+    baseQuery : fetchBaseQuery({
+        baseUrl : import.meta.env.VITE_API_BASE_URL,
+        credentials : 'include'
+    }),
+    tagTypes : ['getStudent'],
+
+    endpoints : (builder) => ({
+        // Endpoint for student registration
+        login : builder.mutation({
+            query : ({type, credentials}) => ({
+                url : `${type}/login`,
+                method : 'POST',
+                body : credentials,
+                credentials : 'include'
+            }),
+            invalidatesTags : ['getStudent'],
+            transformResponse : (response) => ({
+                data : response.data
+            }),
+
+        })
+    })
+})
+
+export const {useLoginMutation} = studentApi;
